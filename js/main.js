86632620b5e4bcf3080c83d118ca100f7fcd1235
ShaxruhAtanazarov <nazarovshaxruh98@gmail.com>
2021-09-26 08:28:48 +0500

    Career-inner complated

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ $('.customers__slider').slick({
     arrows: false,
     dots: true,
 });
+
+$('.detal__link').on('click', function(e) {
+    e.preventDefault();
+
+    $('.tab').removeClass('tab--active');
+    $('.tab-content').removeClass('tab-content--active ');
+
+    $(this).addClass('tab--active');
+    $($(this).attr('href')).addClass('tab-content--active ');
+});
+
+$('.navigation__link').on('click', function(e){
+    $('.navigation__link').removeClass('navigation__link--active');
+    $(this).addClass('navigation__link--active');
+
+});
